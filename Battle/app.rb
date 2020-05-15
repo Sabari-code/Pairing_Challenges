df4750614b5e6f1174cc2870dd8b2eea962ae103
Sabari-code <63646147+Sabari-code@users.noreply.github.com>
2020-05-15 09:54:09 +0100

    Add files via upload

---
+++ Battle/app.rb
@@ -0,0 +1,25 @@
+# in app.rb
+
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+  enable :sessions
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
